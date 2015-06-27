8ec0cafc2dec8f74f5cae0f1c8e7041feceb34dc
= <=>
2015-06-27 09:18:05 +0700

    sql

--- document/database/cloud_services.sql
+++ document/database/cloud_services.sql
@@ -19,13 +19,17 @@ DROP TABLE IF EXISTS `cs_config`;
 
 CREATE TABLE `cs_config` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `theme_style` int(11) DEFAULT NULL,
+  `theme_style` varchar(20) DEFAULT NULL,
   `theme_color` varchar(20) DEFAULT NULL,
   `project` varchar(20) DEFAULT NULL,
+  `activated` tinyint(1) DEFAULT '0',
+  `deleted` tinyint(1) DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 
 /*Data for the table `cs_config` */
+
+insert  into `cs_config`(`id`,`theme_style`,`theme_color`,`project`,`activated`,`deleted`) values (1,'layout','smart_cafe_blue','smart_cafe',0,0),(2,'layout4','light','smart_cafe',1,0),(3,'layout','darkblue','smart_cafe',0,0);
 
 /*Table structure for table `sc_customer` */
 
@@ -43,11 +47,11 @@ CREATE TABLE `sc_customer` (
   `type` tinyint(1) DEFAULT '1',
   `deleted` tinyint(1) DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 /*Data for the table `sc_customer` */
 
-insert  into `sc_customer`(`id`,`customer_name`,`address`,`phone`,`time_created`,`user_created`,`time_modified`,`user_modified`,`type`,`deleted`) values (1,'Home',NULL,NULL,NULL,NULL,NULL,NULL,1,0),(2,'Cus1',NULL,NULL,NULL,NULL,NULL,NULL,1,0);
+insert  into `sc_customer`(`id`,`customer_name`,`address`,`phone`,`time_created`,`user_created`,`time_modified`,`user_modified`,`type`,`deleted`) values (1,'Home','Address 1','123',NULL,NULL,NULL,NULL,1,0),(2,'Cus1','Add1','Phone1',NULL,NULL,NULL,NULL,1,0),(3,'Cus2','Add2','Phone2',NULL,NULL,NULL,NULL,1,0),(4,'Home2','Address 1','123',NULL,NULL,NULL,NULL,1,1),(5,'Home3','Address 1','123',NULL,NULL,NULL,NULL,1,1),(6,'Home1','Address 1','123',NULL,NULL,NULL,NULL,1,1),(7,'Cus3','Add3','Phone3',NULL,NULL,NULL,NULL,1,0),(8,'Cus4','Add4','Phone4',NULL,NULL,NULL,NULL,1,0),(9,'Cus5','Add4','Phone4',NULL,NULL,NULL,NULL,1,0),(10,'Cus6','Add4','Phone4',NULL,NULL,NULL,NULL,1,0),(11,'Cus7','Add4','Phone4',NULL,NULL,NULL,NULL,1,0),(12,'Cus8','Add4','Phone4',NULL,NULL,NULL,NULL,1,0),(13,'Cus9','Add4','Phone4',NULL,NULL,NULL,NULL,1,0),(14,'Cus10','Add4','Phone4',NULL,NULL,NULL,NULL,1,0),(15,'Cus11','Add4','Phone4',NULL,NULL,NULL,NULL,1,0),(16,'Cus12','Add4','Phone4',NULL,NULL,NULL,NULL,1,0),(17,'Cus13','Add4','Phone4',NULL,NULL,NULL,NULL,1,0),(18,'Cus14','Add4','Phone4',NULL,NULL,NULL,NULL,1,0);
 
 /*Table structure for table `sc_group` */
 
@@ -70,7 +74,7 @@ CREATE TABLE `sc_group` (
 
 /*Data for the table `sc_group` */
 
-insert  into `sc_group`(`id`,`group_name`,`admin`,`parent_id`,`customer_id`,`permission`,`time_created`,`user_created`,`time_modified`,`user_modified`,`deleted`) values (1,'Admin',1,0,1,'',NULL,NULL,NULL,NULL,0),(2,'Boss',0,0,2,'{\"1\":{\"view\":1},\"2\":{\"view\":1},\"3\":{\"view\":1},\"4\":{\"view\":1},\"5\":{\"view\":1},\"6\":{\"view\":1},\"7\":{\"view\":1},\"8\":{\"view\":1},\"9\":{\"view\":1},\"10\":{\"view\":1}}',NULL,NULL,NULL,NULL,0),(3,'Cashier',0,2,2,'{\"1\":{\"view\":1},\"2\":{\"view\":1},\"3\":{\"view\":1},\"4\":{\"view\":1},\"5\":{\"view\":1},\"6\":{\"view\":1}}',NULL,NULL,NULL,NULL,0);
+insert  into `sc_group`(`id`,`group_name`,`admin`,`parent_id`,`customer_id`,`permission`,`time_created`,`user_created`,`time_modified`,`user_modified`,`deleted`) values (1,'Admin',1,0,1,'{\"16\":{\"view\":1,\"add\":1,\"edit\":1,\"delete\":1,\"export\":1},\"2\":{\"view\":1},\"3\":{\"view\":1},\"4\":{\"view\":1},\"5\":{\"view\":1},\"6\":{\"view\":1},\"7\":{\"view\":1},\"8\":{\"view\":1},\"9\":{\"view\":1},\"10\":{\"view\":1}}',NULL,NULL,NULL,NULL,0),(2,'Boss',0,0,2,'{\"1\":{\"view\":1},\"2\":{\"view\":1},\"3\":{\"view\":1},\"4\":{\"view\":1},\"5\":{\"view\":1},\"6\":{\"view\":1},\"7\":{\"view\":1},\"8\":{\"view\":1},\"9\":{\"view\":1},\"10\":{\"view\":1}}',NULL,NULL,NULL,NULL,0),(3,'Cashier',0,2,2,'{\"1\":{\"view\":1},\"2\":{\"view\":1},\"3\":{\"view\":1},\"4\":{\"view\":1},\"5\":{\"view\":1},\"6\":{\"view\":1}}',NULL,NULL,NULL,NULL,0);
 
 /*Table structure for table `sc_menu` */
 
@@ -92,11 +96,11 @@ CREATE TABLE `sc_menu` (
   `user_modified` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
   `deleted` tinyint(1) DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 /*Data for the table `sc_menu` */
 
-insert  into `sc_menu`(`id`,`menu_name`,`parent_id`,`route`,`url`,`uri`,`params`,`ordering`,`icon`,`time_created`,`user_created`,`time_modified`,`user_modified`,`deleted`) values (14,'Management',0,'#',NULL,NULL,'view,add,edit,delete,import,export',2,'icon-folder',NULL,NULL,NULL,NULL,0),(15,'General',14,'#',NULL,NULL,'view,add,edit,delete,import,export',1,'icon-folder',NULL,NULL,NULL,NULL,0),(16,'Customer',14,'AdminBundle\\Controller\\CustomerController','sc_admin_customer','/smart-cafe/admin/customer.html','view,add,edit,delete',1,'icon-folder',NULL,NULL,NULL,NULL,0),(17,'Menu',15,'AdminBundle\\Controller\\MenuController','sc_admin_menu','/smart-cafe/admin/menu.html','view,add,edit,delete',2,'icon-folder',NULL,NULL,NULL,NULL,0),(18,'Config',14,'#',NULL,NULL,'view,add,edit,delete,import,export',2,'icon-folder',NULL,NULL,NULL,NULL,0),(20,'Dashboard',0,'#',NULL,NULL,'view',1,'icon-home',NULL,NULL,NULL,NULL,0),(21,'Real time',20,'AdminBundle\\Controller\\DashboardController','sc_admin_dashboard','/smart-cafe/admin/dashboard.html','view,add,edit,delete,import,export',NULL,'icon-folder',NULL,NULL,NULL,NULL,0),(22,'Orders',20,'AdminBundle\\Controller\\DashboardController','sc_admin_order','/smart-cafe/admin/order.html','view,add,edit,delete,import,export',NULL,'icon-folder',NULL,NULL,NULL,NULL,0);
+insert  into `sc_menu`(`id`,`menu_name`,`parent_id`,`route`,`url`,`uri`,`params`,`ordering`,`icon`,`time_created`,`user_created`,`time_modified`,`user_modified`,`deleted`) values (14,'Management',0,'#',NULL,NULL,'view,add,edit,delete,import,export',2,'icon-folder',NULL,NULL,NULL,NULL,0),(15,'General',14,'#',NULL,NULL,'view,add,edit,delete,import,export',1,'icon-folder',NULL,NULL,NULL,NULL,0),(16,'Customer',14,'SmartCafe\\AdminBundle\\Controller\\CustomerController','sc_admin_customer','/smart-cafe/admin/customer.html','view,add,edit,delete',1,'icon-folder',NULL,NULL,NULL,NULL,0),(17,'Product',15,'SmartCafe\\AdminBundle\\Controller\\ProductController','sc_admin_product','/smart-cafe/admin/product.html','view,add,edit,delete',2,'icon-folder',NULL,NULL,NULL,NULL,0),(18,'Config',14,'#',NULL,NULL,'view,add,edit,delete,import,export',2,'icon-folder',NULL,NULL,NULL,NULL,0),(20,'Dashboard',0,'#',NULL,NULL,'view',1,'icon-home',NULL,NULL,NULL,NULL,0),(21,'Real time',20,'SmartCafe\\AdminBundle\\Controller\\DashboardController','sc_admin_dashboard','/smart-cafe/admin/dashboard.html','view,add,edit,delete,import,export',NULL,'icon-folder',NULL,NULL,NULL,NULL,0),(22,'Orders',20,'SmartCafe\\AdminBundle\\Controller\\DashboardController','sc_admin_order','/smart-cafe/admin/order.html','view,add,edit,delete,import,export',NULL,'icon-folder',NULL,NULL,NULL,NULL,0),(23,'Other',15,'#',NULL,NULL,'view,add,edit,delete,import,export',2,'icon-folder',NULL,NULL,NULL,NULL,0),(24,'Theme',23,'SmartCafe\\AdminBundle\\Controller\\ThemeController','sc_admin_theme','/smart-cafe/admin/theme.html','view,add,edit,delete,import,export',1,'icon-folder',NULL,NULL,NULL,NULL,0);
 
 /*Table structure for table `sc_user` */
 
